a0663f4f312a2e010a448e8a1319da546f55cf1b
Stanley <180237506+deskstan24@user.noreplay.github.com>
2025-02-01 15:23:38 +0100

    add some functionaliity to most of the buttons using js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-"use strict";
+let color = "black";
+function populateBoard(size) {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "white";
+    square.addEventListener("mouseover", colorSquare);
+    board.insertAdjacentElement("beforeend", square);
+  }
+}
+populateBoard(16);
+
+function changeSize(input) {
+  if (input >= 2 && input <= 100) {
+    populateBoard(input);
+  } else {
+    console.log("Error");
+  }
+}
+function colorSquare() {
+  this.style.backgroundColor = color;
+}
+
+function changeColor(choice) {
+  color = choice;
+}
